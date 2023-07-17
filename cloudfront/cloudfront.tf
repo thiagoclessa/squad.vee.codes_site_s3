c476b788faab563a1a271264b23c5bc5d910b289
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-07-17 16:39:58 -0300

    Update cloudfront.tf

--- cloudfront/cloudfront.tf
+++ cloudfront/cloudfront.tf
@@ -36,14 +36,10 @@ resource "aws_cloudfront_distribution" "tf" {
       restriction_type = "none"
     }
   }
-
   
   viewer_certificate {
     cloudfront_default_certificate = false
-
-    minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method       = "sni-only"
-
     acm_certificate_arn = "${var.vca}"
   }
 }
