8b96b4605c6b85e7794c4d0b565d66b841e1563e
thiagoclessa <thiagoclessa@gmail.com>
2023-07-18 12:00:18 -0300

    ajuste pipeline

--- template_s3/s3.tf
+++ template_s3/s3.tf
@@ -1,6 +1,12 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket
-
+  website {
+      index_document = "index.html"
+      error_document = "404.html"
+  }
+    tags = {
+    "Name" = "Site"
+  }
 }
 
 #resource "aws_s3_bucket_acl" "bucket_acl" {
