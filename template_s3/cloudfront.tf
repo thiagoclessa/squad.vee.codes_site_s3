9d1c8967fad367390679e96e0a2213f6066d2787
thiagoclessa <thiagoclessa@gmail.com>
2023-07-18 11:44:26 -0300

    ajuste permissão bucket

--- template_s3/cloudfront.tf
+++ template_s3/cloudfront.tf
@@ -61,18 +61,18 @@ resource "aws_cloudfront_distribution" "cloudfront" {
 }
 
 # Create Route53 Record to CloudFront
-#resource "aws_route53_record" "domain_record" {
- # name    = var.route53_zone_domain
- # type    = "A"
- # zone_id = var.id_zone
+resource "aws_route53_record" "domain_record" {
+  name    = "${var.bucket}${var.route53_zone_domain}"
+  type    = "A"
+  zone_id = var.id_zone
 
- # alias {
- #   name                   = aws_cloudfront_distribution.cloudfront.domain_name
- #   zone_id                = aws_cloudfront_distribution.cloudfront.hosted_zone_id
- #   evaluate_target_health = false
- # }
+  alias {
+    name                   = aws_cloudfront_distribution.cloudfront.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront.hosted_zone_id
+    evaluate_target_health = false
+  }
 
-#  depends_on = [
+  depends_on = [
     aws_cloudfront_distribution.cloudfront
-#  ]
-#}
+  ]
+}
