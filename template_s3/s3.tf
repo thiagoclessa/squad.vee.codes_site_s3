dfb25bfb7dfed6b67c40375aadda05e77b69ecc8
thiagoclessa <thiagoclessa@gmail.com>
2023-07-18 12:51:00 -0300

    ajuste bucket

--- template_s3/s3.tf
+++ template_s3/s3.tf
@@ -1,26 +1,17 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket
-  website {
-      index_document = "index.html"
-      error_document = "404.html"
+}
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
   }
-    tags = {
-    "Name" = "Site"
+
+  error_document {
+    key = "404.html"
   }
 }
-
-#resource "aws_s3_bucket_acl" "bucket_acl" {
-#  bucket = aws_s3_bucket.bucket.id
-#  acl    = "public"
-#}
-
-#resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
-#  bucket = aws_s3_bucket.bucket.id
-#  block_public_acls       = true
-#  block_public_policy     = true
-#  ignore_public_acls      = true
-#  restrict_public_buckets = true
-#}
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
