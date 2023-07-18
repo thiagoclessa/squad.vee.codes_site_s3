697d33981b15eca37ea92e701821f823e7538d85
thiagoclessa <thiagoclessa@gmail.com>
2023-07-18 09:45:53 -0300

    ajuste site

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "template_s3" {
   source  = "./template_s3"
   route53_zone_domain              = local.config.domain
   cdn_domain                       = local.config.cdn
-  zone_id                          = local.zone_id
+  id_zone                          = local.id_zone
 }
